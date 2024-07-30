2478e353162622ac825cd107c47be54738cf266e
ogura taiki <ogura@camping-car.co.jp>
2024-07-30 11:48:12 +0900

    simulationとAPI繋ぎこみ

---
+++ common/js/script.js
@@ -0,0 +1,89 @@
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
+
+new Swiper(".swiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+document
+  .getElementById("reservation")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const formData = new FormData(form);
+
+    try {
+      const response = await fetch("http://localhost:3001/inbound-request", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const result = await response.json();
+      console.log(result);
+      alert("Reservation successful!");
+    } catch (error) {
+      console.error("Error:", error);
+      alert("There was a problem with your reservation.");
+    }
+  });
+
+document
+  .getElementById("simulation")
+  .addEventListener("change", async function (e) {
+    e.preventDefault();
+
+    document.getElementById("simulation_price").innerHTML = "";
+    const err_msg = document.getElementById("simulation_msg");
+
+    const form = document.getElementById("simulation");
+    const formData = new FormData(form);
+
+    try {
+      const start_date = new Date(formData.get("start_date"));
+      const end_date = new Date(formData.get("end_date"));
+      const members = parseInt(formData.get("members"), 10);
+      const date_difference = (end_date - start_date) / (1000 * 60 * 60 * 24);
+
+      if (isNaN(start_date.getTime()) || isNaN(end_date.getTime())) {
+        err_msg.innerText = "Please select date";
+        return;
+      }
+
+      if (date_difference <= 5) {
+        err_msg.innerText = "Please select a period of more than 5 days";
+        return;
+      }
+
+      const start_date_str = start_date.toISOString().split("T")[0]; // yyyy-mm-dd
+      const end_date_str = end_date.toISOString().split("T")[0]; // yyyy-mm-dd
+
+      const url = `http://localhost:3001/reservation-global-plan?loan_datetime=${encodeURIComponent(
+        start_date_str
+      )}&return_datetime=${encodeURIComponent(end_date_str)}`;
+
+      const response = await fetch(url);
+      const result = await response.json();
+
+      const total_plan_price = result.reduce((total, item) => {
+        return total + parseInt(item.plan_price, 10);
+      }, 0);
+
+      const facility_fee = members * 45300;
+
+      const total_price = total_plan_price + facility_fee;
+
+      document.getElementById("simulation_price").innerHTML =
+        "¥" + total_price.toLocaleString();
+      err_msg.innerText = ""; // エラーメッセージをクリア
+    } catch (error) {
+      console.error("Error:", error);
+      err_msg.innerText = "An error occurred while calculating the simulation.";
+    }
+  });
